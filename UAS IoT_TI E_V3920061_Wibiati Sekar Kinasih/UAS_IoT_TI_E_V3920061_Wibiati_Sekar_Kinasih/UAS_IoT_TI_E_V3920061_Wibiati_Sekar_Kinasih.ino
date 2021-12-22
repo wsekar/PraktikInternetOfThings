7b7266c7bc5cb3ddea63149fd234931afb06a93e
Wibiati Sekar K <89697985+wsekar@users.noreply.github.com>
2021-12-22 19:39:33 +0700

    Add files via upload

---
+++ UAS IoT_TI E_V3920061_Wibiati Sekar Kinasih/UAS_IoT_TI_E_V3920061_Wibiati_Sekar_Kinasih/UAS_IoT_TI_E_V3920061_Wibiati_Sekar_Kinasih.ino
@@ -0,0 +1,67 @@
+// Present a "Will be back soon web page", as stand-in webserver.
+// 2011-01-30 <jc@wippler.nl>
+//
+// License: GPLv2
+
+#include <EtherCard.h>
+
+#define STATIC 1  // set to 1 to disable DHCP (adjust myip/gwip values below)
+
+#if STATIC
+// ethernet interface ip address
+static byte myip[] = { 192,168,1,200 };
+// gateway ip address
+static byte gwip[] = { 192,168,1,104 };
+#endif
+
+// ethernet mac address - must be unique on your network
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+
+byte Ethernet::buffer[500]; // tcp/ip send and receive buffer
+
+const char page[] PROGMEM =
+"HTTP/1.0 503 My Service \r\n"
+"Content-Type: text/html\r\n"
+"Retry-After: 600\r\n"
+"\r\n"
+"<html>"
+  "<head><title>"
+    "Serv"
+  "</title></head>"
+  "<body>"
+    "<p><em><strong>"
+      " Nama  : Wibiati Sekar Kinasih<br />"
+      " NIM   : V3920061<br />"
+      " Prodi : Teknik Informatika 2020<br />"
+      " Email : wibiatisekark@student.uns.ac.id<br />"
+    "<strong></em></p>"
+  "</body>"
+"</html>"
+;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("\n[backSoon]");
+
+  // Change 'SS' to your Slave Select pin, if you arn't using the default pin
+  if (ether.begin(sizeof Ethernet::buffer, mymac, 10) == 0)
+    Serial.println( "Failed to access Ethernet controller");
+#if STATIC
+  ether.staticSetup(myip, gwip);
+#else
+  if (!ether.dhcpSetup())
+    Serial.println("DHCP failed");
+#endif
+
+  ether.printIp("IP:  ", ether.myip);
+  ether.printIp("GW:  ", ether.gwip);
+  ether.printIp("DNS: ", ether.dnsip);
+}
+
+void loop(){
+  // wait for an incoming TCP packet, but ignore its contents
+  if (ether.packetLoop(ether.packetReceive())) {
+    memcpy_P(ether.tcpOffset(), page, sizeof page);
+    ether.httpServerReply(sizeof page - 1);
+  }
+}
